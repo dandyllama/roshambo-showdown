ef5b13a2dd6db4d2c9d3bc94bdbbb981780b4f41
dandyllama <121185297+dandyllama@users.noreply.github.com>
2023-11-28 12:58:10 +0300

    Add game function

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,6 @@ function getComputerSelection() {
   let computerSelection;
   let randomChoice = Math.random();
 
-  console.log(randomChoice);
-
   if (randomChoice <= 0.33) {
     computerSelection = "rock";
   } else if (randomChoice > 0.66) {
@@ -26,19 +24,66 @@ function compareSelections(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
   if (playerSelection === computerSelection) {
-    return `It's a tie`;
+    return 0;
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return `You win! ${playerSelection} beats ${computerSelection}`;
+    return 1;
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "scissors" && playerSelection === "paper")
+  ) {
+    return -1;
   } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    return undefined;
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerSelection();
-console.log(computerSelection);
-console.log(compareSelections(playerSelection, computerSelection));
+function game() {
+  // play a best-of-five game
+  // track scores
+  // report winner or loser at the end
+
+  let playerScore = 0;
+  let computerScore = 0;
+
+  let count = 1;
+
+  for (count; count <= 5; count++) {
+    let playerSelection = prompt("Choose between Rock, Paper & Scissors");
+    let computerSelection = getComputerSelection();
+    console.log(computerSelection);
+    console.log(compareSelections(playerSelection, computerSelection));
+
+    switch (compareSelections(playerSelection, computerSelection)) {
+      case 0:
+        console.log("It's a tie");
+        break;
+      case 1:
+        playerScore += 1;
+        console.log("You win this round!");
+        console.log(`You now have ${playerScore} points`);
+        break;
+      case -1:
+        computerScore += 1;
+        console.log("You lose this round!");
+        console.log(`The computer now has ${computerScore} points`);
+        break;
+      default:
+        console.log("Illegal entry!");
+    }
+  }
+
+  if (playerScore > computerScore) {
+    console.log("You've won the game!");
+  } else if (playerScore < computerScore) {
+    console.log("You've lost the game!");
+  } else {
+    console.log("The game was a tie.");
+  }
+}
+
+game();
