82e56b0ac6473cb78efdf9098c51284ad1d295be
dandyllama <121185297+dandyllama@users.noreply.github.com>
2023-11-28 12:11:52 +0300

    Add computer selection function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,27 @@
-function getComputerChoice() {
-    //
+function getComputerSelection() {
+  // randomly generate between rock, paper and scissors
+  // return selection
+
+  let computerSelection;
+  let randomChoice = Math.random();
+
+  console.log(randomChoice);
+
+  if (randomChoice <= 0.33) {
+    computerSelection = "rock";
+  } else if (randomChoice > 0.66) {
+    computerSelection = "paper";
+  } else {
+    computerSelection = "scissors";
+  }
+
+  return computerSelection;
+}
+
+console.log(getComputerSelection());
+
+function compareSelections(playerSelection, computerSelection) {
+  // compare player and computer seleciton
+  // replay round if tied
+  // return round result
 }
