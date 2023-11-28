d9b237b84b8df13cfe778280efb61957d8c6f254
dandyllama <121185297+dandyllama@users.noreply.github.com>
2023-11-28 12:26:48 +0300

    Add compare selections function

--- scripts/script.js
+++ scripts/script.js
@@ -18,10 +18,27 @@ function getComputerSelection() {
   return computerSelection;
 }
 
-console.log(getComputerSelection());
-
 function compareSelections(playerSelection, computerSelection) {
   // compare player and computer seleciton
   // replay round if tied
   // return round result
+
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return `It's a tie`;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
 }
+
+const playerSelection = "rock";
+const computerSelection = getComputerSelection();
+console.log(computerSelection);
+console.log(compareSelections(playerSelection, computerSelection));
